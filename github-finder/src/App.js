ef7082686955042ede134febce478615b2b748b9
Frankpeter Ani <frankpeterani@gmail.com>
2020-06-16 09:48:51 +0100

    New Day

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,18 +1,15 @@
-import React, { Fragment, Component } from "react";
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar.js";
 import "./App.css";
 
 class App extends Component {
   render() {
-    const name = "Daze";
-    const foo = () => "Bar";
-
     return (
-      <Fragment>
-        <h1>Hello {name}</h1>
-        <h2>Goodbye {name.toUpperCase()}</h2>
-        <h2>Goodbye {foo()}</h2>
-      </Fragment>
+      <div className='App'>
+        <Navbar />
+      </div>
     );
   }
 }
+
 export default App;
